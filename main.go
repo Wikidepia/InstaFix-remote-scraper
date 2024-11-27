06c1d9e464b4d76a45a874172dbe16f28c300445
Akmal <magchuz@pm.me>
2024-11-28 05:11:47 +0700

    Update graphql params

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ var header = http.Header{
 	"x-fb-friendly-name":          {"PolarisPostActionLoadPostQueryQuery"},
 	"x-ig-app-id":                 {"936619743392459"},
 }
-var postData = "__a=1&__ccg=UNKNOWN&__comet_req=7&__csr=n2Yfg_5hcQAG5mPtfEzil8Wn-DpKGBXhdczlAhrK8uHBAGuKCJeCieLDyExenh68aQAKta8p8ShogKkF5yaUBqCpF9XHmmhoBXyBKbQp0HCwDjqoOepV8Tzk8xeXqAGFTVoCciGaCgvGUtVU-u5Vp801nrEkO0rC58xw41g0VW07ISyie2W1v7F0CwYwwwvEkw8K5cM0VC1dwdi0hCbc094w6MU1xE02lzw&__d=www&__dyn=7xeUjG1mxu1syUbFp40NonwgU7SbzEdF8aUco2qwJw5ux609vCwjE1xoswaq0yE6ucw5Mx62G5UswoEcE7O2l0Fwqo31w9a9wtUd8-U2zxe2GewGw9a362W2K0zK5o4q3y1Sx-0iS2Sq2-azo7u3C2u2J0bS1LwTwKG1pg2fwxyo6O1FwlEcUed6goK2O4UrAwCAxW6Uf9EObzVU8U&__hs=19888.HYP%3Ainstagram_web_pkg.2.1..0.0&__hsi=7380500578385702299&__req=k&__rev=1014227545&__s=trbjos%3An8dn55%3Ayev1rm&__spin_b=trunk&__spin_r=1014227545&__spin_t=1718406700&__user=0&av=0&doc_id=25531498899829322&dpr=2&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisPostActionLoadPostQueryQuery&jazoest=2882&lsd=AVoPBTXMX0Y&server_timestamps=true&variables=%7B%22shortcode%22%3A%22$$POSTID$$%22%7D"
+var postData = "fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisPostActionLoadPostQueryQuery&variables=%7B%22shortcode%22%3A%22$$POSTID$$%22%2C%22fetch_tagged_user_count%22%3Anull%2C%22hoisted_comment_id%22%3Anull%2C%22hoisted_reply_id%22%3Anull%7D&doc_id=8845758582119845"
 
 //go:embed dictionary.bin
 var dict []byte
@@ -96,7 +96,7 @@ func main() {
 	transportCache.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
 		if addr == "www.instagram.com:443" {
 			// IP is geo based, need to add some flag
-			return baseDialFunc(ctx, network, "31.13.95.174:443")
+			return baseDialFunc(ctx, network, "157.240.7.174:443")
 		}
 		return cacheDialCtx(ctx, network, addr)
 	}
@@ -139,7 +139,6 @@ func Scrape(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	data := gjson.Parse(b2s(response)).Get("data")
-
 	if !bytes.Contains(response, []byte("shortcode_media")) {
 		http.Error(w, "Post not found", http.StatusNotFound)
 		return
